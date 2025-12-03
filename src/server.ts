2bf1ada09e79e3e61b2ac1b31fe3d9824c7da944
TarekNexus <muhammadtarek5920@gmail.com>
2025-12-04 04:03:21 +0600

    todos api done

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response } from "express";
+
+import express, { NextFunction, Request, Response } from "express";
 import { Pool } from "pg";
 import dotenv from "dotenv";
 import path from "path";
@@ -45,7 +46,16 @@ const initDB = async () => {
 
 initDB();
 
-app.get("/", (req: Request, res: Response) => {
+
+// logger middleware
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}\n`);
+  next();
+};
+
+
+
+app.get("/", logger,(req: Request, res: Response) => {
   res.send("Hello Tareks World!");
 });
 // users CRUD
@@ -165,6 +175,53 @@ app.delete("/users/:id", async (req: Request, res: Response) => {
     });
   }
 });
+// todos crud
+app.post("/todos", async (req: Request, res: Response) => {
+  const { user_id, title } = req.body;
+  try {
+    const result = await pool.query(
+      `INSERT INTO todos(user_id,title) VALUES($1,$2) RETURNING *`,
+      [user_id, title]
+    );
+    // console.log(result.rows[0]);
+    res.status(201).json({
+      success: true,
+      message: "Data Inserted Successfully",
+      data: result.rows[0],
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+// get all todo
+app.get("/todos", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM todos`);
+    res.status(200).json({
+      success: true,
+      message: "todos retrieved successfully",
+      data: result.rows,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+
+app.use((req,res)=>{
+res.status(404).json({
+    success:false,
+    message:"Route not found",
+    path:req.path
+})
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
