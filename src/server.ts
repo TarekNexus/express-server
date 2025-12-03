bb51679bfff10984d8a54c1490c2d490748e1d7d
TarekNexus <muhammadtarek5920@gmail.com>
2025-12-03 23:38:55 +0600

    code updated

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import express, { Request, Response } from "express";
-import{Pool} from"pg";
-import dotenv from"dotenv"
-import path from "path"
+import { Pool } from "pg";
+import dotenv from "dotenv";
+import path from "path";
 
-dotenv.config({path:path.join(process.cwd(),'.env')})
+dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 const app = express();
 const port = 5000;
@@ -11,14 +11,13 @@ const port = 5000;
 app.use(express.json());
 // app.use(express.urlencoded());
 
-
 // DB
-const pool=new Pool({
-    connectionString:`${process.env.CONNECTION_STR}`
-})
+const pool = new Pool({
+  connectionString: `${process.env.CONNECTION_STR}`,
+});
 // user table
-const initDB=async()=>{
-    await pool.query(`
+const initDB = async () => {
+  await pool.query(`
         CREATE TABLE IF NOT EXISTS users(
         id SERIAL PRIMARY KEY,
         name VARCHAR(100) NOT NULL,
@@ -42,17 +41,35 @@ const initDB=async()=>{
             updated_at TIMESTAMP DEFAULT NOW()
             )
             `);
-  
 };
 
 initDB();
 
-
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello Tareks World!");
 });
-app.post("/", (req: Request, res: Response) => {
-  console.log(req.body);
+
+app.post("/users", async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+  try {
+    const result = await pool.query(
+      `INSERT INTO users(name,email) VALUES($1,$2) RETURNING *`,
+      [name, email]
+    );
+    // console.log(result.rows[0]);
+    res.status(201).json({
+      success: true,
+      message: "Data Inserted Successfully",
+      data:result.rows[0]
+    });
+
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+
   res.status(201).json({
     success: true,
     message: "Tarek vai is comming ........",
