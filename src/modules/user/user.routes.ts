391e5666d28fcea7e866fec0eebba0ad736c8bf6
TarekNexus <muhammadtarek5920@gmail.com>
2025-12-05 04:14:25 +0600

    code updated

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { UserControllers } from './user.controller';
 import logger from '../../middleware/logger';
+import auth from '../../middleware/auth';
 const router=express.Router()
 // app.use("/users",userRoutes)
 router.post("/",UserControllers.createUser);
-router.get("/",logger,UserControllers.getUser)
+router.get("/",logger,auth(),UserControllers.getUser)
 router.get("/:id",UserControllers.getSingleUser)
 router.put("/:id",UserControllers.updatedUser)
 router.delete("/:id",UserControllers.deleteUser)
